7eca52707158e72981c214e7aaa57db1a02c88cc
Jean Corzo <jean.corzo92@gmail.com>
2023-09-24 04:15:11 -0300

    organize dependencies versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.android.ktx)
+    implementation(libs.android.appcompat)
+    implementation(libs.android.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.android.junit)
+    androidTestImplementation(libs.android.espresso)
 }
