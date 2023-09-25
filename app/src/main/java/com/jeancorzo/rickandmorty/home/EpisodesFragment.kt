9d859f93d2c3f31dedca078afbe7a0c9711ed191
Jean Corzo <jean.corzo92@gmail.com>
2023-09-25 18:02:28 -0300

    HomeActivity navigation and fragments

---
+++ app/src/main/java/com/jeancorzo/rickandmorty/home/EpisodesFragment.kt
@@ -0,0 +1,19 @@
+package com.jeancorzo.rickandmorty.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jeancorzo.rickandmorty.R
+
+class EpisodesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_episodes, container, false)
+    }
+
+}
--- app/src/main/java/com/jeancorzo/rickandmorty/home/HomeActivity.kt
+++ app/src/main/java/com/jeancorzo/rickandmorty/home/HomeActivity.kt
@@ -1,12 +1,27 @@
 package com.jeancorzo.rickandmorty.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.jeancorzo.rickandmorty.R
+import com.jeancorzo.rickandmorty.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : FragmentActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        val navController = findNavController(R.id.home_nav_host_fragment)
+        binding.homeBottomNavigation.setupWithNavController(navController)
     }
 }
