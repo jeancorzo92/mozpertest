af8e8531316a9a00ba08d4800babd73b88e02b95
Jean Corzo <jean.corzo92@gmail.com>
2023-10-10 21:29:19 -0300

    Remove files related to episodes and fragments

--- app/src/main/java/com/jeancorzo/rickandmorty/episodes/service/dto/EpisodeDto.kt
+++
@@ -1,9 +0,0 @@
-package com.jeancorzo.rickandmorty.episodes.service.dto
-
-data class EpisodeDto(
-    val id: Int,
-    val name: String,
-    val type: String,
-    val dimension: String,
-    val residents: List<String>
-)
