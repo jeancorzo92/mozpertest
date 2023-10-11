bfc63db920bf5508b9526665147c0781a1c47425
Jean Corzo <jean.corzo92@gmail.com>
2023-10-10 21:41:03 -0300

    Migrate database to version 3 after deleting EpisodeEntity table

--- app/src/main/java/com/jeancorzo/rickandmorty/storage/db/AppDatabase.kt
+++ app/src/main/java/com/jeancorzo/rickandmorty/storage/db/AppDatabase.kt
@@ -2,21 +2,27 @@ package com.jeancorzo.rickandmorty.storage.db
 
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.DeleteTable
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.jeancorzo.rickandmorty.storage.db.dao.CharacterDao
-import com.jeancorzo.rickandmorty.storage.db.dao.EpisodeDao
 import com.jeancorzo.rickandmorty.storage.db.entities.CharacterEntity
-import com.jeancorzo.rickandmorty.storage.db.entities.EpisodeEntity
 
 @Database(
-    entities = [CharacterEntity::class, EpisodeEntity::class],
-    version = 2,
+    entities = [CharacterEntity::class],
+    version = 3,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
     ]
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun characterDao(): CharacterDao
+}
 
-    abstract fun episodeDao(): EpisodeDao
+val MIGRATION_2_3 = object: Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DROP TABLE IF EXISTS EpisodeEntity")
+    }
 }
--- app/src/main/java/com/jeancorzo/rickandmorty/storage/db/di/DatabaseModule.kt
+++ app/src/main/java/com/jeancorzo/rickandmorty/storage/db/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.jeancorzo.rickandmorty.storage.db.di
 
 import androidx.room.Room
 import com.jeancorzo.rickandmorty.storage.db.AppDatabase
+import com.jeancorzo.rickandmorty.storage.db.MIGRATION_2_3
 import org.koin.dsl.module
 
 val databaseModule = module {
@@ -10,6 +11,8 @@ val databaseModule = module {
             get(),
             AppDatabase::class.java,
             "app_database"
-        ).build()
+        )
+            .addMigrations(MIGRATION_2_3)
+            .build()
     }
 }
